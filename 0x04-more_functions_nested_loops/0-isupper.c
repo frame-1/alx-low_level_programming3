e53c1c0e9391e0356cdef66de7271d1ec4a4d3ee
frame-1 <muthee031@gmail.com>
2022-03-17 13:14:42 -0700

    Task

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,16 +3,13 @@
 
 /**
  * main - Check code
- *
- * Return: Always 0
+ *@c: input
+ * Return: 1 if c is uppercase, 0 otherwise
  */
-int main(void)
+int _isupper(int c)
 {
-	char c;
-
-	c = 'A';
-	printf("%c: %d\n", c, _isupper(c));
-	c = 'a';
-	printf("%c: %d\n", c, _isupper(c));
-	return (0);
+	if ((c >= 'A') && (c <= 'Z'))
+		return (1);
+	else
+		return (0);
 }
